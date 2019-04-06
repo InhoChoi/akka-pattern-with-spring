dcce687d5ae802fb0af01ee3708c550d5ea86626
Inho Choi <inhochoi@Inhoui-MacBookPro.local>
2019-04-06 13:36:53 +0900

    First Commit

---
+++ src/main/java/com/inhochoi/springakka/SpringAkkaApplication.java
@@ -0,0 +1,13 @@
+package com.inhochoi.springakka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAkkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAkkaApplication.class, args);
+	}
+
+}
